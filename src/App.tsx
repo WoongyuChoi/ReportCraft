67913637f767949088b787a5c5ba45b2e045ad8a
WoongyuChoi <schna@kakao.com>
2024-11-29 15:34:17 +0900

    feature : chart.js, ChartProvider 환경 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from ".";
 import "./App.css";
+import ChartProvider from "./provider/ChartProvider";
 import ReportProvider from "./provider/ReportProvider";
 
 function App() {
   return (
-    <ReportProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </ReportProvider>
+    <ChartProvider>
+      <ReportProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </ReportProvider>
+    </ChartProvider>
   );
 }
 
